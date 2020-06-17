c703d65e9b35361f31f64949d1e44b290a014ec5
Nathalie Olsson <Nathalieolsson123321@outlook.com>
2020-06-17 12:46:07 +0200

    uppdates

--- reaact-starter-template-master/src/components/App.js
+++ reaact-starter-template-master/src/components/App.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class componentName extends Component {
   render() {
-    return <p>template</p>;
+    return <p>template dfhfghghghd</p>;
   }
 }
